170b815ba71fafc9eaad06ec66179270eb9fa8d9
Enzo Galantino <Enzo2991@live.fr>
2024-07-27 17:39:16 +0200

    Feat: remove playerid

--- clothing.lua
+++ clothing.lua
@@ -2,36 +2,6 @@ local PreviousComponent = {}
 local PreviousTexture = {}
 local PreviousTorso = {}
 
-Config.TakeOff = { -- The ID of the component that will be used when you take off the clothes (these are the vanilla IDs without clothing mods)
-    male = {    
-        shirt = 15,
-        pants = 21,
-        shoes = 34,
-        jewelry = 0,
-        glasses = 0,
-        watch = 2,
-        vest = 0,
-        mask = 0,
-        hat = 11,
-        bag = 0,
-        gloves = 0
-    },
-
-    female = {
-        shirt = 15,
-        pants = 21,
-        shoes = 35,
-        jewelry = 0,
-        glasses = 5,
-        watch = 1,
-        vest = 0,
-        mask = 0,
-        hat = 57,
-        bag = 0,
-        gloves = 0
-    }
-}
-
 local function GetPedGender(playerPed)
     local male = GetHashKey("mp_m_freemode_01")
     local female = GetHashKey("mp_f_freemode_01")
